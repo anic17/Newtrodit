227948f937ff0f82b6645aa7d8be10d0e933023a
anic17 <58483910+anic17@users.noreply.github.com>
2021-01-04 10:45:42 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,9 +1,7 @@
-@set compiler=tcc
 @set existent=0
-@gcc -v > nul 2>&1 && @set "existent=1" && @set compiler=gcc
-@%compiler% -v > nul 2>&1 && @set "existent=1" && @set compiler=tcc
+@tcc -v > nul 2>&1 && @set "existent=1"
 @if "%existent%"=="0" (@echo Could not find any compiler in path) else @(
-	%compiler% newtrodit.c -o newtrodit.exe -O2 -luser32 -m32  || @(echo compilation error&&pause>nul&&exit /b 1)
+	tcc newtrodit.c -o newtrodit.exe -O2 -luser32 -m32  || @(echo compilation error&&pause>nul&&exit /b 1)
 	echo compilation successful
 	pause>nul
 )
