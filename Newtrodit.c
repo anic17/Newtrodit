0fb01bf1ff34dea2a6e0b636e17de5dc51157e2f
anic17 <58483910+anic17@users.noreply.github.com>
2021-01-14 18:07:52 +0100

    Newtrodit 0.2, bug fixes and new features

--- Newtrodit.c
+++ Newtrodit.c
@@ -1,3 +1,25 @@
+/*
+  Copyright 2020 anic17 Software
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+  this software and associated documentation files (the "Software"), to deal in 
+  the Software without restriction, including without limitation the rights to 
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+  the Software, and to permit persons to whom the Software is furnished to do so, 
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all 
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #include <stdio.h>
 #include <Windows.h>
 #include <stdlib.h>
@@ -5,27 +27,36 @@
 #include <conio.h>
 #include <errno.h>
 #include <stdbool.h>
-//#include "newtrodit_core.h"
+#include <locale.h>
 #include "manual.c"
 #include "dialog.h"
-
-#define XSIZE GetConsoleXDimension()
-#define YSIZE GetConsoleYDimension()
-
-
 
 char filename_text[MAX_FILENAME] = "Untitled";
 
 int line = 0;
 int col = 0;
 
+/*
+
+
+
+"Hola"
+
+----------
+
+*/
+
+//XSIZE-((XSIZE/2)+len/2)-1
+
 void CenterFileName(char *filename)
 {
+	SetColor(0x70);
 	int cols = GetConsoleXDimension();
 	gotoxy(0, 0);
-	int center_filename = cols - ((cols / 2) + strlen(filename)) - 1;
+	int center_filename = cols - ((cols / 2) + strlen(filename) / 2) - 1;
 	gotoxy(center_filename, 0);
 	printf("%s", filename);
+	SetColor(0x07);
 }
 
 void ShowBottomMenu()
@@ -62,10 +93,6 @@ void ShowBottomMenu()
 	return;
 }
 
-void SetBackgroundColor()
-{
-	SetColor(0x07);
-}
 void CursorSettings(int visible, int size)
 {
 	HANDLE Cursor = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -88,28 +115,24 @@ void NewtroditNameLoad()
 	printf(" Newtrodit %s", newtrodit_version);
 }
 
-void BS()
+void BottomLineInput(const char *bottom_string)
 {
-	putchar(' ');
-	putchar('\b');
-	putchar('\b');
-	putchar(' ');
-	putchar('\b');
+	SetColor(0x07);
+	ClearLine(GetConsoleYDimension() - 1);
+	printf("%s", bottom_string);
 }
 
-
-int PrintBottomString(char* bottom_string)
+void BS()
 {
-	gotoxy(0, GetConsoleYDimension()-1);
-	printf("%s", bottom_string);
-	return errno;
+	putchar('\b');
+	putchar(' ');
+	putchar('\b');
 }
 
 int QuitProgram(int posx, int posy)
 {
-	int rows = GetConsoleYDimension();
-	gotoxy(1, rows - 1);
-	SetColor(0x70);
+	ClearLine(GetConsoleYDimension()-1);
+	gotoxy(0, GetConsoleYDimension()-1);
 	printf("%s", NEWTRODIT_PROMPT_QUIT);
 	int confirmquit = getch();
 	if (confirmquit == 89 || confirmquit == 121)
@@ -127,14 +150,33 @@ int QuitProgram(int posx, int posy)
 
 		ShowBottomMenu();
 		gotoxy(posx, posy);
-		SetBackgroundColor();
+		SetColor(0x07);
 	}
 	return 0;
 }
 
+void LoadAllNewtrodit()
+{
+	ClearScreen();
+	SetColor(0x07);
+	NewtroditNameLoad();
+	CenterFileName(filename_text);
+
+	ShowBottomMenu();
+
+	SetColor(0x07);
+	CursorSettings(TRUE, 20);
+}
+
 int main(int argc, char *argv[])
 {
-	char str_save[][] = {0};
+	bool isSaved = false;
+	int codepage_save = GetConsoleOutputCP();
+	char str_save[512][1024] = {};
+	for (int i = 0; i < YSIZE; i++)
+	{
+		str_save[i][0] = '\0';
+	}
 	int edit_file = 0;
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	WORD start_color;
@@ -143,7 +185,7 @@ int main(int argc, char *argv[])
 	{
 		start_color = csbi.wAttributes;
 	}
-
+	int lines_paste = 0;
 	int cols = GetConsoleXDimension();
 	int rows = GetConsoleYDimension();
 	if (cols < 54 || rows < 6)
@@ -151,36 +193,50 @@ int main(int argc, char *argv[])
 		MessageBox(0, NEWTRODIT_ERROR_WINDOW_TOO_SMALL, "Newtrodit", 16);
 		return 0;
 	}
+	int argc_shift = 1;
 	if (argc > 1)
 	{
-		if (!strcmp(argv[1], "--help"))
+		if (!strcmp(argv[argc_shift], "--help"))
 		{
 			NewtroditHelp();
 			getch();
 			SetColor(start_color);
 			exit(0);
 		}
+		if (!strcmp(argv[argc_shift], "--utf8"))
+		{
+			setlocale(LC_ALL, "");
+		}
+		argc_shift++;
 	}
-	if (argc == 2)
+	int open_argv_x, open_argv_y;
+	if (argc == argc_shift + 1)
 	{
 		FILE *newtrodit_write_argv;
-		newtrodit_write_argv = fopen(argv[1], "r");
+		newtrodit_write_argv = fopen(argv[argc_shift], "r");
 		if (!newtrodit_write_argv)
 		{
-			printf("arg: %s: %s", argv[1], strerror(errno));
+			printf("arg: %s: %s", argv[argc_shift], strerror(errno));
 			exit(EXIT_FAILURE);
 		}
 		int c_read;
 		gotoxy(0, 1);
-		strcpy(filename_text, argv[1]);
+		strcpy(filename_text, argv[argc_shift]);
+
 		while ((c_read = getc(newtrodit_write_argv)) != EOF)
 		{
+			str_save[open_argv_y][open_argv_x++] = c_read;
+			if (c_read == 10 || c_read == 13)
+			{
+				open_argv_y++;
+			}
 			putchar(c_read);
 		}
 		getch();
 		SetColor(start_color);
-		return EXIT_SUCCESS;
 	}
+
+	size_t end_key_len;
 	int biggest_y_line = 1;
 	char save_destination[MAX_FILENAME];
 	int show_cursor_pos_window_size, save_file_dialog;
@@ -188,19 +244,11 @@ int main(int argc, char *argv[])
 	int find_substring_index;
 	int find_substring_len;
 	char *basenameopen;
+	char *final_paste_strrchr;
 	char fileopenread[512];
 	char findstring[8192];
-	ClearScreen();
-
+	LoadAllNewtrodit();
 	int xpos = 0, ypos = 1;
-	SetColor(0x07);
-	NewtroditNameLoad();
-	CenterFileName(filename_text);
-
-	ShowBottomMenu();
-
-	SetBackgroundColor();
-	CursorSettings(TRUE, 20);
 
 	gotoxy(xpos, ypos);
 
@@ -212,31 +260,20 @@ int main(int argc, char *argv[])
 	SetColor(0x07);
 	while (1)
 	{
-		show_cursor_pos_window_size = GetConsoleYDimension();
-		gotoxy(0, show_cursor_pos_window_size - 1);
+
+		//show_cursor_pos_window_size = GetConsoleYDimension();
+		gotoxy(0, GetConsoleYDimension() - 1);
 		for (int i = 0; i < 17; ++i)
 		{
 			putchar(' ');
 		}
-		gotoxy(0, show_cursor_pos_window_size - 1);
+		gotoxy(0, GetConsoleYDimension() - 1);
 		printf("Ln %d, Col %d", ypos, xpos + 1);
 		gotoxy(xpos, ypos);
 
 		ch = getch();
 		str[index++] = ch;
 
-		if (ch == 8)
-		{
-			if (xpos > 0)
-			{
-				xpos--;
-				index--;
-			}
-			else
-			{
-				Alert();
-			}
-		}
 		if (ch == 15)
 		{
 			FILE *fileread;
@@ -255,7 +292,7 @@ int main(int argc, char *argv[])
 				getch();
 				ShowBottomMenu();
 				gotoxy(xpos, ypos);
-				SetBackgroundColor();
+				SetColor(0x07);
 			}
 			else
 			{
@@ -264,7 +301,7 @@ int main(int argc, char *argv[])
 
 			strcpy(filename_text, fileopenread);
 			CenterFileName(filename_text);
-			SetBackgroundColor();
+			SetColor(0x07);
 			printf("yeee");
 			xpos = 0, ypos = 1;
 			gotoxy(xpos, ypos);
@@ -287,10 +324,8 @@ int main(int argc, char *argv[])
 
 		if (ch == 3) // ^C
 		{
-
-			size_t len = strlen(str) + 1;
-			HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
-			memcpy(GlobalLock(hMem), str, len);
+			HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strlen(str_save[ypos]) + 1);
+			memcpy(GlobalLock(hMem), str_save[ypos], strlen(str_save[ypos]) + 1);
 			GlobalUnlock(hMem);
 			OpenClipboard(0);
 			EmptyClipboard();
@@ -299,69 +334,71 @@ int main(int argc, char *argv[])
 			continue;
 		}
 
-		if (ch == 19) // ^S = Save
-		{
-		}
-		if (ch == 8)
-		{
-			xpos--;
-			putchar(' ');
-		}
-
 		if (ch == 7) // ^G = Goto line
 		{
 
 			int console_cols_gotoxy = GetConsoleXDimension();
-			gotoxy(0, GetConsoleYDimension()-1);
-			for(int i = 0; i < console_cols_gotoxy-1; ++i)
+			gotoxy(0, GetConsoleYDimension() - 1);
+			for (int i = 0; i < console_cols_gotoxy - 1; ++i)
 			{
 				putchar(' ');
 			}
 
-			gotoxy(0, GetConsoleYDimension()-1);	
+			gotoxy(0, GetConsoleYDimension() - 1);
 			printf("%s", NEWTRODIT_PROMPT_GOTO_LINE);
 			char line_number_str[6];
 			int line_number_chr = 0, line_number_chr_index = 0;
 
-			while(line_number_chr != 13)
+			while (line_number_chr != 13)
 			{
 				line_number_chr = getch();
-				if(line_number_chr > 47 && line_number_chr < 58)
+				if (line_number_chr > 47 && line_number_chr < 58)
 				{
-					if(atoi(line_number_str) >= 10000)
+					if (atoi(line_number_str) >= 10000)
 					{
 						Alert();
-					} else {
+					}
+					else
+					{
 						line_number_str[line_number_chr_index++] = line_number_chr;
+						putchar(line_number_chr);
 					}
 				}
-				if(line_number_chr == 8)
+				if (line_number_chr == 8)
 				{
+					line_number_str[line_number_chr_index--] = '\0';
 					BS();
-					line_number_chr_index--;
+				}
+				if (line_number_chr == 27)
+				{
+					ShowBottomMenu();
+					gotoxy(xpos, ypos);
+					break;
 				}
 			}
-			printf("%s", line_number_str);
-			getch();
-			exit(0);
-			if(atoi(line_number_str) < GetConsoleYDimension())
+			if (atoi(line_number_str) > GetConsoleYDimension() - 3)
 			{
-				gotoxy(0, GetConsoleYDimension()-1);
-				printf("%s", NEWTRODIT_ERROR_CON_BUFFER_LIMIT);
+				ClearLine(GetConsoleYDimension() - 1);
+				PrintBottomString(NEWTRODIT_ERROR_GOTO_BUFFER_SIZE);
+				getch();
+				ShowBottomMenu();
 			}
-			ypos = atoi(line_number_str) ;
-			gotoxy(0, ypos);
-			printf("%d:%d", xpos, ypos);
-			ShowBottomMenu();
-			
-
+			else
+			{
+				gotoxy(0, atoi(line_number_str));
+				xpos = -1;
+				ypos = atoi(line_number_str);
+				ShowBottomMenu();
+			}
 		}
 		if (ch == 224)
 		{
 			ch = getch();
 			switch (ch)
 			{
+
 			case 72:
+
 				// Up arrow
 				if (ypos > 1)
 				{
@@ -382,15 +419,33 @@ int main(int argc, char *argv[])
 
 			case 80:
 				// Down arrow
-				if((ypos+1) >= 27)
+				if ((ypos + 1) >= 27)
 				{
 					Alert();
-				} else {
+				}
+				else
+				{
 					gotoxy(xpos, ++ypos);
 				}
-				
+
+				break;
+
+			case 71:
+				// HOME key
+				xpos = 0;
+				gotoxy(xpos, ypos);
+				break;
+			case 79:
+				// END key
+				end_key_len = strlen(str_save[ypos]);
+				xpos = end_key_len;
+				gotoxy(end_key_len, ypos);
+				break;
+			case 83:
+				// DEL key
 				break;
 			}
+
 			continue;
 			ch = 0;
 		}
@@ -401,16 +456,25 @@ int main(int argc, char *argv[])
 			index = 0;
 
 			xpos = 0;
-			str_save[ypos++][xpos] = '\n';
+			ypos++;
+			str_save[ypos][xpos] = '\n';
 			printf("\n");
-			if(biggest_y_line < ypos)
+			if (biggest_y_line < ypos)
 			{
 				biggest_y_line++;
 			}
 			continue;
 		}
-		if (ch == 6)
+
+		if (ch == 6) // ^F
 		{
+			ClearLine(GetConsoleYDimension() - 1);
+			PrintBottomString("Find string function unavailable at the moment.");
+			getch();
+			ClearLine(GetConsoleYDimension() - 1);
+			ShowBottomMenu();
+			continue;
+
 			gotoxy(1, YSIZE);
 			printf("%s", NEWTRODIT_PROMPT_FIND_STRING);
 			fgets(findstring, sizeof findstring, stdin);
@@ -423,7 +487,7 @@ int main(int argc, char *argv[])
 				gotoxy(0, ypos);
 				gotoxy(find_substring_index, 2);
 				SetColor(0x0e);
-				printf("yellow");
+				printf("yellow for find ^F");
 			}
 			// _! continue;
 		}
@@ -441,82 +505,125 @@ int main(int argc, char *argv[])
 				QuitProgram(xpos, ypos);
 				SetColor(0x07);
 			}
-			// _! continue
 			if (ch == 59)
 			{
 				NewtroditHelp();
 				getch();
 			}
 		}
-		if (ch == 19)
+		if (ch == 19) // ^S
 		{
-			save_file_dialog = GetConsoleYDimension();
-
-			gotoxy(0, save_file_dialog);
-			for (int i = 0; i < save_file_dialog; ++i)
-			{
-				putchar(' ');
-			}
-			gotoxy(0, save_file_dialog);
-			printf("%s", NEWTRODIT_PROMPT_SAVE_FILE);
-			fgets(save_destination, sizeof save_destination, stdin);
-			size_t len_savedest = strlen(save_destination);
-			save_destination[len_savedest - 1] = '\0';
 			FILE *fp_savefile;
-			fp_savefile = fopen(save_destination, "w");
-
-
-			if (!fp_savefile)
+			if (isSaved == false)
 			{
-				gotoxy(0, save_file_dialog);
-				for (int i = 0; i < save_file_dialog; ++i)
+				ClearLine(GetConsoleYDimension() - 1);
+				PrintBottomString(NEWTRODIT_PROMPT_SAVE_FILE);
+
+				fgets(save_destination, sizeof save_destination, stdin);
+				size_t len_savedest = strlen(save_destination);
+				save_destination[len_savedest - 1] = '\0';
+				fp_savefile = fopen(save_destination, "wb");
+
+				if (!fp_savefile)
 				{
-					putchar(' ');
+					ClearLine(GetConsoleYDimension() - 1);
+					BottomLineInput(strerror(errno));
+					continue;
 				}
-				gotoxy(0, save_file_dialog);
-				printf("%s: %s", save_destination, strerror(errno));
-				continue;
+				isSaved = true;
 			}
 
 			int save_index_x = 0, save_index_y = 1; // Save index
-			ClearScreen();
-			for(int i = 0; i < ypos; ++i)
+			//SetConsoleOutputCP(65001);
+			for (int i = 1; i < YSIZE; i++)
 			{
-				printf("CHAR: \"%s\"\n", str_save[i]);
+				if (str_save[i] != NULL)
+				{
+					fprintf(fp_savefile, "%s\n", str_save[i]);
+				}
 			}
-
-			/* 
-			while(str_save[save_index_y] == NULL)
-			{
-				//for(int i = 0; i < )
-				fprintf(stdout, "%s", str_save[save_index_y]);
-			}
-			*/
+			NewtroditNameLoad();
+			CenterFileName(strlasttok(save_destination, '\\'));
 			fclose(fp_savefile);
+			ShowBottomMenu();
+			//SetConsoleOutputCP(codepage_save);
 		}
 
 		if (ch == 22)
 		{
-			char *buffer;
+
+			char *buffer_clipboard;
 			if (OpenClipboard(0))
 
 			{
-				buffer = (char *)GetClipboardData(CF_TEXT);
-				printf("%s", buffer);
-			}
+				buffer_clipboard = (char *)GetClipboardData(CF_TEXT);
+				printf("%s", buffer_clipboard);
+				final_paste_strrchr = strrchr(buffer_clipboard, '\n');
 
+				lines_paste = 0;
+				for (int i = 0; i < strlen(buffer_clipboard); i++)
+				{
+					if (buffer_clipboard[i] == '\n')
+					{
+						lines_paste++;
+					}
+				}
+				xpos += strlen(final_paste_strrchr + 1);
+				ypos += lines_paste;
+			}
 			CloseClipboard();
 			// _! continue;
 		}
-		xpos++;
-		str_save[ypos][xpos] = ch;
-		if(ch == 8)
+		if (ch == 27)
 		{
-			BS();
-		} else {
-			putchar(ch);
+			printf("\"%s\"", str_save[ypos]);
 		}
-		
+
+		if (ch == 8)
+		{
+			if (xpos > 0)
+			{
+
+				str_save[ypos][xpos + 1] = '\0';
+				printf("\b \b");
+				xpos -= 2;
+				
+			}
+			else
+			{
+				xpos--;
+				//xpos-=2;
+				if (ypos < 2)
+				{
+					Alert();
+					xpos -= 1;
+				}
+				else
+				{
+					/* Act as END key */
+					
+						str_save[ypos][xpos-1] = '\0';
+						end_key_len = strlen(str_save[ypos - 1]);
+						printf("strlen(%d), \"%s\"", end_key_len, str_save[ypos]);
+						xpos = end_key_len - 1;
+						gotoxy(end_key_len, --ypos);
+					
+				}
+			}
+		}
+		else
+		{
+			if (ch > 31)
+			{
+				str_save[ypos][xpos] = ch;
+				putchar(ch);
+			}
+			else
+			{
+				xpos--;
+			}
+		}
+		xpos++;
 	}
 	SetColor(start_color);
 	return 0;
