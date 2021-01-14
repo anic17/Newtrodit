f3d3681146a2382dc6ee68884632b15a976af587
anic17 <58483910+anic17@users.noreply.github.com>
2021-01-14 18:06:02 +0100

    Updated dialogs

--- dialog.h
+++ dialog.h
@@ -1,20 +1,21 @@
-const char NEWTRODIT_ERROR_GOTO_LINE_BIGGER[] = "Specified line is biggest than maximum arrived.";
-const char NEWTRODIT_ERROR_MEMORY_OVERFLOW[] = "Internal memory overflow.";
-const char NEWTRODIT_ERROR_BUFFER_OVERFLOW[] = "Internal buffer overflow.";
-const char NEWTRODIT_ERROR_CON_BUFFER_LIMIT[] = "Console buffer arrived to limit."
+char NEWTRODIT_ERROR_MEMORY_OVERFLOW[100] = "Internal memory overflow.";
+char NEWTRODIT_ERROR_BUFFER_OVERFLOW[100] = "Internal buffer overflow.";
+char NEWTRODIT_ERROR_INTERNAL[100] = "Internal Newtrodit error.";
+char NEWTRODIT_ERROR_CON_BUFFER_LIMIT[100] = "Console buffer arrived to limit.";
+char NEWTRODIT_ERROR_GOTO_BUFFER_SIZE[100] = "Console buffer too small for the specified line.";
 // File IO dialogs
-const char NEWTRODIT_FS_FILE_TOO_BIG[] = "File too big: ";
-const char NEWTRODIT_FS_FILE_CORRUPTED[] = "File corrupted and/or unreadable: ";
-const char NEWTRODIT_FS_FILE_SAVED[] = "File has been successfully saved";
-const char NEWTRODIT_FS_FILE_OPEN_ERR5[] = "Deny of access while trying to open the file";
-const char NEWTRODIT_FS_FILE_SAVE_ERR5[] = "Deny of access while trying to save the file";
-const char NEWTRODIT_FS_FILE_NOT_FOUND[] = "File not found: ";
+char NEWTRODIT_FS_FILE_TOO_BIG[100] = "File too big: ";
+char NEWTRODIT_FS_FILE_CORRUPTED[100] = "File corrupted and/or unreadable: ";
+char NEWTRODIT_FS_FILE_SAVED[100] = "File has been successfully saved";
+char NEWTRODIT_FS_FILE_OPEN_ERR5[100] = "Deny of access while trying to open the file";
+char NEWTRODIT_FS_FILE_SAVE_ERR5[100] = "Deny of access while trying to save the file";
+char NEWTRODIT_FS_FILE_NOT_FOUND[100] = "File not found: ";
 
-const char NEWTRODIT_ERROR_WINDOW_TOO_SMALL[] = "Console window is too small. Please resize it.";
+char NEWTRODIT_ERROR_WINDOW_TOO_SMALL[100] = "Console window is too small. Please resize it.";
 
 // Prompts
-const char NEWTRODIT_PROMPT_QUIT[] = "Are you sure you want to quit? (y/n)";
-const char NEWTRODIT_PROMPT_FOPEN[] = "File to open: ";
-const char NEWTRODIT_PROMPT_GOTO_LINE[] = "Line number: ";
-const char NEWTRODIT_PROMPT_FIND_STRING[] = "String to find: ";
-const char NEWTRODIT_PROMPT_SAVE_FILE[] = "File to save: ";
+char NEWTRODIT_PROMPT_QUIT[100] = "Are you sure you want to quit? (y/n)";
+char NEWTRODIT_PROMPT_FOPEN[100] = "File to open: ";
+char NEWTRODIT_PROMPT_GOTO_LINE[100] = "Line number: ";
+char NEWTRODIT_PROMPT_FIND_STRING[100] = "String to find: ";
+char NEWTRODIT_PROMPT_SAVE_FILE[100] = "File to save: ";
