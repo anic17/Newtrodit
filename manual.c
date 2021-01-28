77c5e2ee149335167eb8f64e8cd5fd505f5127db
anic17 <58483910+anic17@users.noreply.github.com>
2021-01-28 20:37:34 +0100

    Fixed some bugs, still broken

--- manual.c
+++ manual.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
-#include <Windows.h>
 #include "newtrodit_core.h"
-// Define constants
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-#define DISABLE_NEWLINE_AUTO_RETURN  0x0008
-#define MAX_FILENAME 8192
 
 const char manual_file[] = "newtrodit.man";
-int NewtroditHelp()
-{
-	int variable_dollar_character;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	int columns;
 
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+void TopHelpBar()
+{
 	ClearScreen();
 	gotoxy(0,0);
     SetColor(0x70);
-	for(int n = 0; n < columns; n++)
-	{
-		printf(" ");
-	}
+	ClearLine(0);
 	gotoxy(0,0);
 	printf("Newtrodit help");
+	SetColor(0x07);
+}
+
+
+int NewtroditHelp()
+{
+	int variable_dollar_character;
+
+	TopHelpBar();
+	gotoxy(0,0);
+
 	SetColor(0x07);
 
    DWORD l_mode;
@@ -37,40 +34,49 @@ int NewtroditHelp()
 	manual = fopen(manual_file, "rb");
 	if(!manual)
 	{
-		printf("%s: %s", manual_file, strerror(errno));
+		char error_manual_not_found[512];
+		strcpy(error_manual_not_found, manual_file);
+		strcat(error_manual_not_found, ": ");
+		strcat(error_manual_not_found, strerror(errno));
+		PrintBottomString(error_manual_not_found);
 		return errno;
 	}
-	int finished_manual = 0;
-	while(finished_manual == 0)
-	{
-		printf("%d:%d", GetConsoleXDimension(), GetConsoleYDimension());
+	int finished_manual = 1;
+	char manual_buffer[8192];
 
-		for(int i = 0; i < GetConsoleYDimension()-4; i++)
+	int y_console_size = YSIZE;
+	while(fgets(manual_buffer, sizeof manual_buffer, manual))
+	{
+
+		for(int i = 0; i < GetConsoleYDimension()-1; i++)
 		{
-			if((manual_read_buffer = getc(manual))!= EOF)
+			
+			//printf("it's being executed\n");
+			if(fgets(manual_buffer, sizeof manual_buffer, manual))
 			{
-				if(manual_read_buffer == 36) // $
-				{
-					variable_dollar_character = getc(manual);
-					if(variable_dollar_character == 86)
-					{
-						printf("%s", newtrodit_version);
-					}
-				}
-				printf("%c", manual_read_buffer);
-			} else {
-				finished_manual = 1;
+				printf("%s", manual_buffer);
 			}
+			
+			
+
 		}
-		gotoxy(0, GetConsoleYDimension());
-		printf("^X  Close help  M-F4 Quit Newtrodit\r");
+
+		SetColor(0x70);
+		ClearLine(y_console_size-1);
+
+		PrintBottomString("^X  Close help  M-F4 Quit Newtrodit\r");
+		SetColor(0x07);
 		getch();
-		for(int i = 0; i < GetConsoleXDimension()-1; i++);
-		{
-			printf(" ");
-		}
-		
+		TopHelpBar();
+		y_console_size+YSIZE;
+
 
 	}
+
+	
+	return 10;
+
+
+	
 	
 }
