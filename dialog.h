bd7831bf17b39d39e4f526d06c16196db1445c86
anic17 <58483910+anic17@users.noreply.github.com>
2021-07-16 13:10:41 +0200

    Add files via upload

--- dialog.h
+++ dialog.h
@@ -1,30 +1,35 @@
-char NEWTRODIT_ERROR_MEMORY_OVERFLOW[65] = "Internal memory overflow.";
-char NEWTRODIT_ERROR_BUFFER_OVERFLOW[65] = "Internal buffer overflow.";
-char NEWTRODIT_ERROR_INTERNAL[65] = "Internal Newtrodit error.";
-char NEWTRODIT_ERROR_CON_BUFFER_LIMIT[65] = "Console buffer arrived to limit.";
-char NEWTRODIT_ERROR_GOTO_BUFFER_SIZE[65] = "Console buffer too small for the specified line.";
-char NEWTRODIT_ERROR_INVALID_XPOS[65] = "Invalid X position for cursor.";
-char NEWTRODIT_ERROR_INVALID_YPOS[65] = "Invalid Y position for cursor.";
-char NEWTRODIT_ERROR_CALLOC_FAILED[65] = "calloc() failed. Program cannot start.";
-char NEWTRODIT_ERROR_BUFFER_ALLOCATION[65] = "Buffer allocation failed. Program cannot start.";
-char NEWTRODIT_WARNING_RESIZED[65] = "Warning: Newtrodit window resized.";
+// Errors and warnings
+
+char NEWTRODIT_ERROR_BUFFER_ALLOCATION[] = "Buffer allocation failed. Program cannot start.";
+char NEWTRODIT_ERROR_BUFFER_OVERFLOW[] = "Internal buffer overflow.";
+char NEWTRODIT_ERROR_CALLOC_FAILED[] = "Allocation function calloc() failed. Program cannot start.";
+char NEWTRODIT_ERROR_CLIPBOARD_COPY[] = "Error while copying string to clipboard.";
+char NEWTRODIT_ERROR_CON_BUFFER_LIMIT[] = "Console buffer arrived to limit.";
+char NEWTRODIT_ERROR_GOTO_BUFFER_SIZE[] = "Console buffer too small for the specified line.";
+char NEWTRODIT_ERROR_INTERNAL[] = "Internal Newtrodit error.";
+char NEWTRODIT_ERROR_INVALID_XPOS[] = "Invalid X position for cursor.";
+char NEWTRODIT_ERROR_INVALID_YPOS[] = "Invalid Y position for cursor.";
+char NEWTRODIT_ERROR_MEMORY_OVERFLOW[] = "Internal memory overflow.";
+char NEWTRODIT_ERROR_WINDOW_TOO_SMALL[] = "Console window is too small. Please resize it.";
+char NEWTRODIT_WARNING_RESIZED[] = "Warning: Newtrodit window resized.";
 
 // File IO dialogs
-char NEWTRODIT_FS_FILE_TOO_BIG[65] = "File too big: ";
-char NEWTRODIT_FS_FILE_CORRUPTED[65] = "File corrupted and/or unreadable: ";
-char NEWTRODIT_FS_FILE_SAVED[65] = "File has been successfully saved.";
-char NEWTRODIT_FS_FILE_OPEN_ERR[65] = "Deny of access while trying to open the file.";
-char NEWTRODIT_FS_FILE_SAVE_ERR[65] = "Deny of access while trying to save the file.";
-char NEWTRODIT_FS_FILE_NOT_FOUND[65] = "File not found: ";
-char NEWTRODIT_ERROR_WINDOW_TOO_SMALL[65] = "Console window is too small. Please resize it.";
-
+char NEWTRODIT_FS_FILE_TOO_BIG[] = "File too big: ";
+char NEWTRODIT_FS_FILE_CORRUPTED[] = "File corrupted and/or unreadable: ";
+char NEWTRODIT_FS_FILE_OPEN_ERR[] = "Deny of access while trying to open the file.";
+char NEWTRODIT_FS_FILE_SAVE_ERR[] = "Deny of access while trying to save the file.";
+char NEWTRODIT_FS_FILE_NOT_FOUND[] = "File not found: ";
 
 // Prompts
-char NEWTRODIT_PROMPT_QUIT[65] = "Are you sure you want to quit? (y/n)";
-char NEWTRODIT_PROMPT_FOPEN[65] = "File to open: ";
-char NEWTRODIT_PROMPT_GOTO_LINE[65] = "Line number: ";
-char NEWTRODIT_PROMPT_FIND_STRING[65] = "String to find: ";
-char NEWTRODIT_PROMPT_SAVE_FILE[65] = "File to save: ";
+
+char NEWTRODIT_PROMPT_FIND_STRING[] = "String to find: ";
+char NEWTRODIT_PROMPT_FOPEN[] = "File to open: ";
+char NEWTRODIT_PROMPT_GOTO_LINE[] = "Line number: ";
+char NEWTRODIT_PROMPT_OVERWRITE[] = "File already exists. Overwrite? (y/n)";
+char NEWTRODIT_PROMPT_QUIT[] = "Are you sure you want to quit? (y/n)";
+char NEWTRODIT_PROMPT_SAVE_FILE[] = "File to save: ";
+char NEWTRODIT_PROMPT_SAVE_FILE_AS[] = "Save file as: ";
+
 
 // Informational dialogas
-char NEWTRODIT_FILE_SAVED[65] = "File saved successfully";
+char NEWTRODIT_FILE_SAVED[] = "File saved successfully.";
