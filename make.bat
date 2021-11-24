564bef0a1f4075edfd74bfe003b73ceaa7e7093d
anic17 <58483910+anic17@users.noreply.github.com>
2021-11-24 13:59:44 +0100

    Added more compilers (gcc and TDM-gcc)

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @set existent=0
-@tcc -v > nul 2>&1 && @set "existent=1"
+@for %%a in (gcc tdm-gcc tcc) do @(%%a -v > nul 2>&1 && set "existent=1" && set "compiler=%%a")
 @if "%existent%"=="0" (@echo Could not find any compiler in path) else @(
-	tcc newtrodit.c -o newtrodit.exe -O2 -luser32 -lkernel32 -m32  || @(echo compilation error&&pause>nul&&exit /b 1)
+	%compiler% newtrodit.c -o newtrodit.exe -O2 -luser32 -lkernel32 -m32  || @(echo compilation error&&pause>nul&&exit /b 1)
 	echo compilation successful
 	pause>nul
 )
