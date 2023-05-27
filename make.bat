08130aef8dee23127acd409a8e552caba0983369
anic17 <58483910+anic17@users.noreply.github.com>
2023-05-27 22:39:27 +0200

    Delete make.bat

--- make.bat
+++
@@ -1,7 +0,0 @@
-@set existent=0
-@tcc -v > nul 2>&1 && @set "existent=1"
-@if "%existent%"=="0" (@echo Could not find any compiler in path) else @(
-	tcc newtrodit.c -o newtrodit.exe -O2 -luser32 -lkernel32 -m32  || @(echo compilation error&&pause>nul&&exit /b 1)
-	echo compilation successful
-	pause>nul
-)
